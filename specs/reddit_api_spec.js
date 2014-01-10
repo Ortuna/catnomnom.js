8f94b960210846c1bf9d40bd70eaeccbf58d9750
Sumeet Singh <ortuna@gmail.com>
2014-01-10 11:54:27 -0500

    More idomatic refactoring

--- specs/reddit_api_spec.js
+++ specs/reddit_api_spec.js
@@ -5,36 +5,49 @@ var api    = require('../lib/reddit_api')
 	, fs     = require('fs')
 	, xhr, requests;
 
-describe('Reddit API', function() {
-	var mockRequest = function(url, return_status, return_value) {
-		nock('http://www.reddit.com')
-		 .get(url)
-		 .reply(return_status, return_value);
-	};
+//Fixtures
+var setupFixtures = function() {
+	nock('http://www.reddit.com')
+	 .get('/r/cats_from_file.json')
+	 .replyWithFile(200, process.cwd() + "/specs/fixtures/cats.json");
 
-	var readFixture = function(path) {
-		var cwd = process.cwd();
-		return JSON.parse(fs.readFileSync(cwd + "/specs/fixtures/cats.json"));
-	};
+	nock('http://www.reddit.com')
+	 .get('/r/cats_empty.json')
+	 .reply(200,{data: {children: { data: {}}}});
+
+	nock('http://www.reddit.com')
+	 .get('/r/cats_invalid.json')
+	 .reply(200, "Some invalid string");
+};
+
+describe('Reddit API', function() {
+	setupFixtures();
+
+	var client;
+	before(function(){
+		client = new api('http://www.reddit.com');
+	});
 
 	it('can query a URL', function(done) {
-		mockRequest('/r/cats.json', 200, {data: {children:{}}});
-		api.query('http://www.reddit.com/r/cats.json', function(cats) {
+		client.query('cats_empty', function(cats) {
 			done();
 		});
 	});
 
 	it('can get JSON data from request', function(done) {
-		example_kitties = readFixture('cats.json');
-
-		mockRequest('/r/cats.json', 200, example_kitties);
-		api.query('http://www.reddit.com/r/cats.json', function(cats) {
+		client.query('cats_from_file', function(cats) {
 			assert.equal(cats.length, 26);
-			assert.equal(cats[0].kind,    "t3");
-			assert.equal(cats[0].data.id, "1sk7uv");
+			assert.equal(cats[0].id,  "1sk7uv");
 			done();
 		});
 	});
 
+	it('returns an empty array when there is a bad response', function(done) {
+		client.query('cats_invalid', function(cats) {
+			assert.equal(cats.length, 0);
+			done();
+		});
+	});
 
 });
+
